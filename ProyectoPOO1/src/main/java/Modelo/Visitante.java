1f98fb9cf2b715a5e24fb4393069ea129d81d8af
ypluas@espol.edu.ec <ypluas@espol.edu.ec>
2021-08-16 12:01:50 -0500

    Se ha añadido la clase Visita y Visitante

---
+++ ProyectoPOO1/src/main/java/Modelo/Visitante.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Visitante {
+    
+    private ArrayList<Visita> visitas;
+    private String cedula;
+    private String nombre;
+    private String correo;
+   
+    public Visitante(String cedula,String nombre,String correo){
+        this.cedula=cedula;
+        this.nombre=nombre;
+        this.correo= correo;
+        visitas = new ArrayList<>();
+    }
+    
+    public String getCedula() {
+        return cedula;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+}
